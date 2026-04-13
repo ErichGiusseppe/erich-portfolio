4f5d2d1fce02d19fd8c62c3d861c19d5d8c533d5
ErichGiusseppe <erichgiusseppesotop@gmail.com>
2026-04-12 22:17:49 -0500

    Block Google Translate: translate=no + notranslate class + meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,8 +46,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`dark ${inter.variable} ${notoSerif.variable} ${dmSans.variable}`}>
+    <html lang="en" translate="no" className={`dark notranslate ${inter.variable} ${notoSerif.variable} ${dmSans.variable}`}>
       <head>
+        <meta name="google" content="notranslate" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
